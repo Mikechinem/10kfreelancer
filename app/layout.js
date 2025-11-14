053c867e6ae54f826a6d6ec659d09628fe29a67f
Mikechinem <Michael Ehumadu_michael.ehumadu@gmail.com>
2025-11-14 22:54:29 +0100

    update layout page

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,16 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import ClientLayout from "@/components/ClientLayout";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "10X Your Productivity",
   description: "Portfolio and services by Michael Ehumadu",
 };
+
+// vercel web speed here >>>
+     <SpeedInsights/>
+
 
 export default function RootLayout({ children }) {
   return (
