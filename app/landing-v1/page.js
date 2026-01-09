bab6b0491167160e16de3684db9877fdae040ae7
Mikechinem <Michael Ehumadu_michael.ehumadu@gmail.com>
2026-01-09 10:33:58 +0100

    update pages and designs

---
+++ app/landing-v1/page.js
@@ -0,0 +1,12 @@
+'use client';
+
+import CTAForm from "@/components/CTAForm";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>Landing Page v1</h1>
+      <CTAForm />
+    </div>
+  );
+}
